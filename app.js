f055282c7e4b9288509a9d567874139cf346b2b5
Luan <158096687+luangeo@users.noreply.github.com>
2024-02-06 00:31:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('java', 'ruby', 'GoLang');
+
+let lista1 = ['arroz', 'feijão', 'macarrão'];
+console.log(lista1[0]);
+
+let lista2 = ['banana', 'uva', 'pera'];
+console.log(lista2[1]);
+
+let lista3 = ['galinha', 'elefante', 'cachorro'];
+console.log(lista3[2]);
